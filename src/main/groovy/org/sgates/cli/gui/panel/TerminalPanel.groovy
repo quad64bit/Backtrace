342659754d6aec92c066db6ac2a71674a319d5e9
Steffen Gates <sgates@ctacorp.com>
2015-01-18 21:47:34 -0500

    Added support for basic insertion point. Added rendering of command history. Added command history. Put boot process back in.

--- src/main/groovy/org/sgates/cli/gui/panel/TerminalPanel.groovy
+++ src/main/groovy/org/sgates/cli/gui/panel/TerminalPanel.groovy
@@ -2,7 +2,6 @@ package org.sgates.cli.gui.panel
 
 import org.sgates.cli.Global
 import org.sgates.cli.gui.panel.structure.PrimaryPanel
-import org.sgates.cli.gui.panel.structure.RenderablePanel
 
 import java.awt.Color
 import java.awt.Graphics2D
@@ -11,7 +10,7 @@ import java.awt.RenderingHints
 /**
  * Created by sgates on 1/13/15.
  */
-class TerminalPanel extends PrimaryPanel implements RenderablePanel{
+class TerminalPanel extends PrimaryPanel{
     private Color backgroundColor = Color.BLACK
     org.sgates.cli.Console console
 
@@ -21,19 +20,49 @@ class TerminalPanel extends PrimaryPanel implements RenderablePanel{
     }
 
     void renderForeground(Graphics2D g){
-        g.setColor(Color.red)
-        g.drawString("Time: ${System.currentTimeMillis()}", 20, 50)
         renderPrompt(g)
+        renderHistory(g)
     }
 
     void renderOverlay(Graphics2D g){
-
+        consoleText(g)
+        if(("${System.currentTimeMillis()}"[10..-1] as int) > 500) {
+            g.drawString((" " * (console.insertionPoint + 2)) + "█", 20, canvas.height - 20)
+        }
     }
 
-    void renderPrompt(Graphics2D g){
+    private void renderHistory(Graphics2D g){
+        consoleText(g)
+        console.commandHistory.recent.eachWithIndex{ line, index ->
+            g.drawString(line, 20, 30+ 24*index)
+        }
+    }
+
+    private void renderPrompt(Graphics2D g){
+        consoleText(g)
+        g.drawString("\$ ${console?.currentLine}", 20, canvas.height - 20)
+    }
+
+    private void consoleText(Graphics2D g){
         g.setColor(Color.CYAN)
         g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
-        g.setFont(Global.consoleFont)
-        g.drawString("\$ ${console?.currentLine}", 20, canvas.height - 20)
+        g.setFont(Global.CONSOLE_FONT)
+    }
+
+    @Override
+    void println(String text) {
+        if(text == ""){
+            console.commandHistory.add("")
+        }
+        StringBuffer sb = new StringBuffer(text)
+        def lines = sb.readLines()
+        lines.each { line ->
+            console.commandHistory.add(line)
+        }
+    }
+
+    @Override
+    void print(String text) {
+        console.commandHistory.append(text)
     }
 }
